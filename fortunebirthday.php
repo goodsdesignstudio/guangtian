41b35a555c3dd605891a081d68b5837cf499b2d7
goodsdesignstudio <design@goods-design.com.tw>
2026-07-30 19:07:53 +0800

    法會活動：流程按鈕位置標記

--- fortunebirthday.php
+++ fortunebirthday.php
@@ -837,23 +837,18 @@
 
 						<div class="flow-sliderWrap is-red">
 
+							<!-- 法會活動輪播按鈕位置 -->
 							<ul class="flow-sliderList flk-btn-2026" data-flickity='{
 
-								"prevNextButtons": true,
-
+															"prevNextButtons": true,
 								"pageDots": true,
-
 								"wrapAround": true,
-
 								"autoPlay": 4000,
-
 								"cellAlign": "center",
-
 								"imagesLoaded": true,
-
-								
-
+			
 							}'>
+								<!--  -->
 
 								<li>
 
